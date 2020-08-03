4a96cfb00fd26e08ebd8228fbba1fa7f5744e1e7
chorseng <39493602+chorseng@users.noreply.github.com>
2020-08-04 01:57:17 +0800

    Create __init__.py

---
+++ lib/train/__init__.py
@@ -0,0 +1,4 @@
+from lib.train.intention_train import intention_train
+from lib.train.recommend_train import recommend_train
+from lib.train.text_train import text_train
+from lib.train.knowledge_train import knowledge_train
