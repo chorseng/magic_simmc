969d3f22cafc359efa70fe3b0ca4037e22bb0fb6
chorseng <39493602+chorseng@users.noreply.github.com>
2020-08-04 02:41:21 +0800

    Create __init__.py

---
+++ lib/valid/__init__.py
@@ -0,0 +1,6 @@
+
+from lib.valid.intention_valid import intention_valid
+from lib.valid.text_valid import text_valid
+from lib.valid.recommend_valid import recommend_valid
+from lib.valid.knowledge_valid import knowledge_valid
+
