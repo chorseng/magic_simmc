3d41b0f78995072bb63bf533b65c2ffb06aa2fc5
chorseng <39493602+chorseng@users.noreply.github.com>
2020-08-04 03:10:22 +0800

    Update __init__.py

--- lib/test/__init__.py
+++ lib/test/__init__.py
@@ -1,6 +1,5 @@
 from lib.test.text_test import text_test
 from lib.test.intention_test import intention_test
 from lib.test.recommend_test import recommend_test
-from lib.test.knowledge_styletip_test import knowledge_styletip_test
-from lib.test.knowledge_attribute_test import knowledge_attribute_test
-from lib.test.knowledge_celebrity_test import knowledge_celebrity_test
+from lib.test.knowledge_test import knowledge_test
+
