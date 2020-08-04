8fa1841e49e6d69be64f9d09600e07ce09b650bc
chorseng <39493602+chorseng@users.noreply.github.com>
2020-08-04 14:56:53 +0800

    Update knowledge_data.py

--- dataset/knowledge_data.py
+++ dataset/knowledge_data.py
@@ -8,95 +8,6 @@ from config import DatasetConfig
 from util import load_pkl, save_pkl
 
 
-class StyleTipsData:
-    def __init__(self, vocab=None, edges=None, scores=None):
-        # Attributes.
-        self.vocab: Dict[str, int] = vocab
-        self.edges: List[Tuple[int, int]] = edges
-        self.scores: List[float] = scores
-
-    def __repr__(self):
-        res = ''
-        res += 'vocab: ' + self.vocab.__repr__() + '\n'
-        res += 'edges: ' + self.edges.__repr__() + '\n'
-        res += 'scores: ' + self.scores.__repr__() + '\n'
-        return res
-
-    @staticmethod
-    def from_file():
-        """Build a StyleTipsData object from file."""
-        if not isfile(DatasetConfig.styletips_data_file):
-            raise ValueError('No style tips data file.')
-
-        vocab: Dict[str, int] = {}
-        edges: List[Tuple[int, int]] = []
-        scores: List[float] = []
-
-        with open(DatasetConfig.styletips_data_file) as file:
-            for line in file:
-                products: List[str] = [None] * 2
-                products[0], products[1], score = map(lambda x: x.strip(),
-                                                      line.split(','))
-                products = list(map(lambda x: x.lower(), products))
-                score = int(score)
-                for product in products:
-                    if product not in vocab:
-                        vocab[product] = len(vocab)
-                edges.append((vocab[products[0]], vocab[products[1]]))
-                scores.append(score)
-
-        return StyleTipsData(vocab, edges, scores)
-
-
-class CelebrityData:
-    def __init__(self, celebrity_id=None, product_id=None, scores=None):
-        self.celebrity_id: Dict[str, int] = celebrity_id
-        self.product_id: Dict[str, int] = product_id
-        self.scores = scores
-
-    def __repr__(self):
-        res = ''
-        res += 'celebrity_id: ' + self.celebrity_id.__repr__() + '\n'
-        res += 'product_id: ' + self.product_id.__repr__() + '\n'
-        res += 'scores: ' + self.scores.__repr__() + '\n'
-        return res
-
-    @staticmethod
-    def from_file():
-        """Build a CelebrityData object from file."""
-        if not isfile(DatasetConfig.celebrity_data_file):
-            raise ValueError('No celebrity data file.')
-
-        celebrity_id: Dict[str, int] = {}
-        product_id: Dict[str, int] = {}
-
-        # Load JSON file.
-        with open(DatasetConfig.celebrity_data_file) as file:
-            celebrity_json = json.load(file)
-
-        # Count celebrities and product types.
-        for celebrity, products in celebrity_json.items():
-            celebrity = celebrity.lower()
-            # Assign ids for new celebrity.
-            if celebrity not in celebrity_id:
-                celebrity_id[celebrity] = len(celebrity_id)
-            for product in products.keys():
-                product = product.lower()
-                if product not in product_id:
-                    product_id[product] = len(product_id)
-
-        scores = [[0] * len(product_id) for _ in range(len(celebrity_id))]
-        for celebrity, products in celebrity_json.items():
-            celebrity = celebrity.lower()
-            cel_id = celebrity_id[celebrity]
-            for product, score in products.items():
-                product = product.lower()
-                prod_id = product_id[product]
-                scores[cel_id][prod_id] = score
-
-        return CelebrityData(celebrity_id, product_id, scores)
-
-
 class AttributeData:
     def __init__(self, key_vocab=None, value_vocab=None):
         self.key_vocab: Dict[str, int] = key_vocab
@@ -153,19 +64,13 @@ class KnowledgeData:
         if isfile(DatasetConfig.knowledge_data_file):
             # Read existed extracted data files.
             knowledge_data = load_pkl(DatasetConfig.knowledge_data_file)
-            self.styletips_data = knowledge_data.styletips_data
-            self.celebrity_data = knowledge_data.celebrity_data
             self.attribute_data = knowledge_data.attribute_data
         else:
             # Load data from raw data file and save them into pkl.
-            self.styletips_data = StyleTipsData.from_file()
-            self.celebrity_data = CelebrityData.from_file()
             self.attribute_data = AttributeData.from_file()
             save_pkl(self, 'KnowledgeData', DatasetConfig.knowledge_data_file)
 
     def __repr__(self):
         res = ''
-        res += 'styletips_data: ' + self.styletips_data.__repr__() + '\n'
-        res += 'celebrity_data: ' + self.celebrity_data.__repr__() + '\n'
         res += 'attribute_data: ' + self.attribute_data.__repr__() + '\n'
         return res
